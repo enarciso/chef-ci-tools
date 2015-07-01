e415c45318022259a00e3308691aea9e21e05d41
knifeofdreams <judit.ordog-andrasi@prezi.com>
2015-07-01 12:27:16 +0200

    [chefspec] chefspec script updated based on foodcritic changes

--- bin/chef-chefspec-publisher.sh
+++ bin/chef-chefspec-publisher.sh
@@ -1,4 +1,4 @@
-#!/bin/bash -x
+#!/bin/bash
 
 # Do not fail to parse cookbooks because of the encoding
 export LC_CTYPE=en_US.UTF-8
@@ -12,19 +12,30 @@ if [ ! -d junit_reports ]; then
   mkdir -p junit_reports
 fi
 
+rm junit_reports/chefspec-*.xml 2>/dev/null
+
 PATH=${HOME}/bin:${PATH}
-#RSPEC=${RSPEC:-rspec} # figure this out
 if [ -s ${HOME}/.rvm/scripts/rvm ]
 then
     . "$HOME/.rvm/scripts/rvm" # Load RVM into a shell session *as a function*
 fi
 
-GIT_PREVIOUS_COMMIT=${1}
-GIT_COMMIT=${2}
+if [ -z `git diff --name-only ${GIT_PREVIOUS_COMMIT} ${GIT_COMMIT} | awk '$1 ~ /^cookbooks/' | awk -F'/' '$3 == "spec"' | awk -F'/' '{print $2}' | uniq` ]; then
+    cat > junit_reports/chefspec-dummy.xml <<EOF
+<?xml version="1.0" encoding="UTF-8"?>
+<testsuite name="rspec" tests="1" failures="0" errors="0" time="0.001334" timestamp="2015-07-01T12:16:36+02:00">
+  <!-- Randomized with seed 55589 -->
+  <properties/>
+  <testcase classname="cookbooks.dummy" name="monitoring f.ing runs" file="" time="0.001192"/>
+</testsuite>
+EOF
+
+    exit 0
+fi
 
 # git submodule cookbooks: git submodule | awk '{print $2}' | awk '$1 ~ /^cookbooks/' | sed -e 's/cookbooks\///'
 for cbname in `git diff --name-only ${GIT_PREVIOUS_COMMIT} ${GIT_COMMIT} | awk '$1 ~ /^cookbooks/' | awk -F'/' '$3 == "spec"' | awk -F'/' '{print $2}' | uniq`; do
-  rm junit_reports/chefspec-${cbname}.xml 2>/dev/null
+  `git checkout ${2}`
   echo "------ chefspec checks: $cbname ------"
   rspec cookbooks/${cbname} --format RspecJunitFormatter --out junit_reports/chefspec-${cbname}.xml
 done
